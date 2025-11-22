fee24a5c9a5f936c36f976ea8a4443b27c19a033
sunshijie <3070863284@qq.com>
2025-11-22 11:10:25 +0800

    feat:推荐指数权重枚举修改

--- yunblog-common/src/main/java/com/ssj/yunblog/common/enums/RecommendWeightEnum.java
+++ yunblog-common/src/main/java/com/ssj/yunblog/common/enums/RecommendWeightEnum.java
@@ -1,24 +1,34 @@
 package com.ssj.yunblog.common.enums;
 
+import lombok.Getter;
+
 /**
  * 推荐指数权重
  *
  * @author: yunkun
  * @Date: 2025/11/22
  */
+@Getter
 public enum RecommendWeightEnum {
 
-    HIGH(3.0),
-    MEDIUM(2.0),
-    LOW(1.0);
+    HIGH("high", 3.0),
+    MEDIUM("medium", 2.0),
+    LOW("low", 1.0);
 
     private final double value;
+    private final String code;
 
-    RecommendWeightEnum(double value) {
+    RecommendWeightEnum(String code, double value) {
         this.value = value;
+        this.code = code;
     }
 
-    public double getValue() {
-        return value;
+    public static RecommendWeightEnum getByCode(String code) {
+        for (RecommendWeightEnum weightEnum : RecommendWeightEnum.values()) {
+            if (weightEnum.code.equals(code)) {
+                return weightEnum;
+            }
+        }
+        return null;
     }
 }
